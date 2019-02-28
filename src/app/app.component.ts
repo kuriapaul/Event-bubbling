2ef46792b461c5449790edfa6a1bd984a89fe560
kuriapaul <pauwains@gmail.com>
2019-02-28 13:12:58 +0300

    created a directive to stop event bubbling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Eventbubbling';
+
+  parentClicked() {
+    alert('Div clicked');
+  }
+
+  childClicked() {
+   
+    alert('Button clicked');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClickStopPropagationDirective } from './click-stop-propagation.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClickStopPropagationDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
